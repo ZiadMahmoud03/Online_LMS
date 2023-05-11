ae17dbf53c6d2426565506810853ccff4b799cf7
Tamerihab <109035523+Tamerihab@users.noreply.github.com>
2023-05-12 00:44:12 +0300

    First Commit

---
+++ src/main/resources/com/example/online_lms/Person.java
@@ -0,0 +1,12 @@
+package com.example.online_lms;
+
+public class Person {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ src/main/resources/com/example/online_lms/StudentTest.java
@@ -0,0 +1,7 @@
+package com.example.online_lms;
+
+public class StudentTest {
+
+	
+
+}
