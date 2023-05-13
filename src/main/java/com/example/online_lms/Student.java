b508fe29fe9fb053a3eae832912563206daebb7d
Tamer Ihab <tamerihab02@gmail.com>
2023-05-13 22:32:10 +0200

    .

--- src/main/java/com/example/online_lms/Student.java
+++ src/main/java/com/example/online_lms/Student.java
@@ -2,4 +2,9 @@ package com.example.online_lms;
 
 public class Student {
 
+    public static void main(String[] args){
+
+        System.out.println("Fuck this");
+    }
+
 }
---
+++ test/com/example/online_lms/StudentTest.java
@@ -0,0 +1,18 @@
+package com.example.online_lms;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    public void test(){
+
+        String str="Hey";
+
+        assertEquals("Hey",str);
+
+    }
+
+}
