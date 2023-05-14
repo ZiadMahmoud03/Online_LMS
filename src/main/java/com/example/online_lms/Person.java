2e8c8b1ff3259dbcb518504d36fc70a18ab26c6f
Tamer Ihab <tamerihab02@gmail.com>
2023-05-14 17:52:25 +0200

    Updated Person Class

--- src/main/java/com/example/online_lms/Person.java
+++ src/main/java/com/example/online_lms/Person.java
@@ -1,11 +1,104 @@
 package com.example.online_lms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+    String FirstName;
+    String LastName;
+    String PhoneNumber;
+    String email;
+    String gender;
 
-	}
+    String address;
+    String dob;
+    String department;
+
+    Long NationalId;
+
+    List<String> Courses;
+
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Long getNationalId() {
+        return NationalId;
+    }
+
+    public void setNationalId(Long nationalId) {
+        NationalId = nationalId;
+    }
+
+    public List<String> getCourses() {
+        return Courses;
+    }
+
+    public void setCourses(List<String> courses) {
+        Courses = courses;
+    }
+
 
 }
